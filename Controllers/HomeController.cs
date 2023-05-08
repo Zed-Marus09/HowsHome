0392239ed9c6a4ffaaaf405b9b12412fcce07edf
VV104 <VV104@CCSLAB526U46>
2023-05-08 09:00:43 +0800

    _Layout and navbar Home, About and PropertyList Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using HowsHome.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HowsHome.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+		
+		public IActionResult PropertyList()
+		{
+			return View();
+		}
+
+        public IActionResult PropertyType()
+        {
+            return View();
+        }
+
+        public IActionResult PropertyAgent()
+        {
+            return View();
+        }
+
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+
+        public IActionResult Error404()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
